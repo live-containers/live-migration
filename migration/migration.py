f5816087bbcc8dbd5ebb14230dd1bdd4a13a1b4b
Carlos Segarra <carlossegarragonzalez@gmail.com>
2020-02-18 19:40:54 +0100

    local migration working properly now, changed the directory the restore command was executed in

--- migration/migration.py
+++ migration/migration.py
@@ -9,7 +9,8 @@ class CustomException(Exception):
     pass
 
 def eprint(*args, **kwargs):
-    print(*args, file=sys.stderr, **kwargs)
+    pre = "Error!"
+    print("{} {}".format(pre, *args), file=sys.stderr, **kwargs)
 
 def check_root():
     if os.geteuid() != 0:
@@ -19,9 +20,9 @@ def check_root():
 def usage():
     txt = "Usage: {} \n".format(sys.argv[0])
     txt += "\t--name <container id>\t\tname of the runC container\n"
-    txt += "\t--restore-dir [dir|pwd]\t\tscp-like path where to restore "
+    txt += "\t--restore-dir <dir>\t\tscp-like path where to restore "
     txt += "the container\n"
-    txt += "\t--container-path [dir|pwd]\t\tpath where the container image is\n"
+    #txt += "\t--container-path [dir|pwd]\t\tpath where the container image is\n"
     txt += "\t--diskless [bool|false]\t\tperform a diskless migration\n" 
     #txt += "[leave-running] [pre-copy|post-copy]\n"
     eprint(txt)
@@ -30,6 +31,7 @@ def usage():
 def parse_kwargs(arg_list):
     ret = {}
     if "--name" not in arg_list:
+        eprint("Missing mandatory argument --name!")
         usage()
     try:
         index = arg_list.index("--name")
@@ -38,7 +40,7 @@ def parse_kwargs(arg_list):
         if ret["container-name"] not in tmp_out:
             raise CustomException
     except CustomException:
-        eprint("Error! {} is not a running container!".format(
+        eprint("{} is not a running container!".format(
               ret["container-name"]))
         sys.exit(1)
     except:
@@ -57,17 +59,28 @@ def parse_kwargs(arg_list):
         try:
             index = arg_list.index("--restore-dir")
             ret["restore-dir"] = arg_list[index + 1]
+            ret["restore-type"] = "local"
             if (ret["restore-dir"][-1] != "/"):
                 ret["restore-dir"] += "/"
-#            if !os.path.exists(ret["restore-dir"]):
-#                eprint("{} is not a valid restore path!".format(
-#                      ret["restore-dir"]))
-#                raise CustomException
+            ret["restore-parent"] = ret["restore-dir"].rsplit('/',2)[0]
+            if not os.path.exists(ret["restore-parent"]):
+                eprint("{} is not a valid restore path!".format(
+                      ret["restore-dir"]))
+                raise CustomException
+            if not os.path.exists(ret["restore-parent"] + "/config.json"):
+                tmp_str = "{} has not got the sufficient ".format(
+                        ret["container-path"])
+                tmp_str += "files to restore the container!"
+                eprint(tmp_str)
+                raise CustomException
         except CustomException:
             sys.exit(1)
         except:
-            eprint("Error! In the restore-dir command")
+            eprint("In the restore-dir command")
             usage()
+    else:
+        eprint("Missing mandatory argument --restore-dir!")
+        usage()
     if "--container-path" in arg_list:
         try:
             index = arg_list.index("--container-path")
@@ -79,7 +92,7 @@ def parse_kwargs(arg_list):
                       ret["restore-dir"]))
                 raise CustomException
             if not os.path.exists(ret["container-path"] + "config.json"):
-                tmp_str = "Error! {} has not got the sufficient ".format(
+                tmp_str = "{} has not got the sufficient ".format(
                         ret["container-path"])
                 tmp_str += "files to restore the container!"
                 eprint(tmp_str)
@@ -87,40 +100,45 @@ def parse_kwargs(arg_list):
         except CustomException:
             sys.exit(1)
         except:
-            eprint("Error! In the container path argument.")
+            eprint("In the container path argument.")
             usage()
     return ret
 
-def pre_cpr_checks(**kwargs):
-    # Check if user has supplied a restore dir that already has a config.json
-    # If it's not the same the container has been generated with we assume
-    # the error (runc will raise it)
+def transfer_checkpoint(**kwargs):
+    # Only transfer if remote! Otherwise use --image-path flag.
+    checkpoint_path = kwargs["container-path"] + "checkpoint"
     if (("restore-dir" in kwargs) and 
-            (not os.path.exists(kwargs["restore-dir"] + "config.json"))):
+            (os.path.exists(checkpoint_path))):
         try:
-            new_dir = "{}{}".format(kwargs["restore-dir"],
-                    kwargs["container-path"].split('/')[-2])
             print("LOG: Transferring files to restore-dir...")
-            shutil.copytree(kwargs["container-path"], new_dir)
+            shutil.copytree(checkpoint_path, kwargs["container-path"])
             print("LOG: Finished transferring files to restore-dir.")
         except (shutil.Error, OSError) as e:
-            eprint("Error! Couldn't transfer files. {}".format(e))
+            eprint("Couldn't transfer files. {}".format(e))
             sys.exit(1)
 
 def migration(**kwargs):
-    # Sanity Checks
-    pre_cpr_checks(**kwargs)
     # Checkpoint
-    cmd = "sudo runc checkpoint {}".format(kwargs["container-name"])
+    cmd_cp = "sudo runc checkpoint {} ".format(kwargs["container-name"])
+    cmd_rs = "sudo runc restore "
+    if (kwargs["restore-type"] == "local"):
+        cmd_cp += " --image-path {} ".format(kwargs["restore-dir"])
+        cmd_rs += " --image-path {} ".format(kwargs["restore-dir"])
+    else:
+        # TODO remote migration
+        #transfer_checkpoint(**kwargs)
+        pass
+    print("Starting checkpoint with the following arguments:")
+    print(kwargs)
     start = time.time()
-    p = subprocess.Popen(cmd, shell=True)
+    p = subprocess.Popen(cmd_cp, shell=True)
     p.wait()
     print("Checkpoint finished!")
     cp_time = time.time()
-    cmd = "sudo runc restore -d {}-restored ".format(kwargs["container-name"])
-    cmd += "&> /dev/null < /dev/null"
-    #p = subprocess.Popen(cmd, shell=True)
-    #p.wait()
+    cmd_rs += "-d {}-restored &> /dev/null < /dev/null".format(
+            kwargs["container-name"])
+    p = subprocess.Popen(cmd_rs, shell=True, cwd=kwargs["restore-parent"])
+    p.wait()
     rt_time = time.time()
     print("Restore finished!")
     print("Time elapsed: \n\t- Checkpoint: {}\n\t- Restore: {}".format(
