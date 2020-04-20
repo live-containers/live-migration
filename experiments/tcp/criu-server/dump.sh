85fbc36426c0347a68aff1d76bbeee41761de258
Carlos Segarra <carlossegarragonzalez@gmail.com>
2020-04-20 09:43:42 +0200

    verh pair in restore

--- experiments/tcp/criu-server/dump.sh
+++ experiments/tcp/criu-server/dump.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 SERVER_PPID=$(ps j `pidof -s server` | awk 'NR>1 {print $1}')
 echo $SERVER_PPID
-sudo criu dump \
+sudo criu dump -v4 \
     -t $SERVER_PPID \
     --images-dir images/1 \
+    --work-dir logs \
     --shell-job \
     --tcp-established
--- experiments/tcp/criu-server/restore.sh
+++ experiments/tcp/criu-server/restore.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo criu restore \
+sudo criu restore -v4 \
     --images-dir images/1 \
+    --work-dir logs \
     --shell-job \
+    --external veth\[veth1\]:br-veth1@br1 \
     --tcp-established
